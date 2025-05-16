758aac99e8e6def3034ac641870380ef74859f59
David Burgos <dburgos834@gmail.com>
2025-05-16 03:30:46 -0600

    Init commit
    Initial commit with standard configurations

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Milicom's Eccomerce",
+  description: "This is a tech assest for Milicom's Full Stack Developer position",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  // Agrega el atributo suppressHydrationWarning para evitar el error
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
